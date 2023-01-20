c1e76d325c9a8e11b74559f88f36b204df3ea39d
Milton Chang <mchangpi@gmail.com>
2023-01-20 20:31:33 +0800

    L34 Quick Overview - Importance of Logging with Spring Boot

--- src/main/java/com/miltontest/springboot/miltonfirstwebapp/login/LoginController.java
+++ src/main/java/com/miltontest/springboot/miltonfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.miltontest.springboot.miltonfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+  private Logger logger = LoggerFactory.getLogger(getClass());
+
   @RequestMapping("login")
   public String logInPage(@RequestParam String name, ModelMap model) {
     model.put("name", name);
-    System.out.println("Request param is " + name); // NOT for production use
+    logger.debug("Rquest param is {}", name);
+    //System.out.println("Request param is " + name); // NOT for production use
     return "login";
   }
 }
