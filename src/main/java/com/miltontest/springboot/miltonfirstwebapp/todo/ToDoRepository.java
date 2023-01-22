5ff6d134e62070261eeaf9d10dcd9e9c704ae150
Milton Chang <mchangpi@gmail.com>
2023-01-22 18:31:08 +0800

    L68 Connecting All Todo App Features to H2 Database

--- src/main/java/com/miltontest/springboot/miltonfirstwebapp/todo/ToDoRepository.java
+++ src/main/java/com/miltontest/springboot/miltonfirstwebapp/todo/ToDoRepository.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ToDoRepository extends JpaRepository<ToDo, Long>{
+public interface ToDoRepository extends JpaRepository<ToDo, Integer>{
   public List<ToDo> findByUsername(String username);
 }
